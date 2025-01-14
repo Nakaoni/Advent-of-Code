7f3254b74a44d9bdab8b80daf4cbb80f33fd376e
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-01-14 22:20:12 +0100

    RED: write test part 1 of day 2

--- 2024/test/fr/nakaoni/aoc/day02/Day02Test.java
+++ 2024/test/fr/nakaoni/aoc/day02/Day02Test.java
@@ -1,4 +1,4 @@
-package fr.nakaoni.aoc.day01;
+package fr.nakaoni.aoc.day02;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,31 +11,18 @@ import fr.nakaoni.aoc.AdventOfCodeApplicationTest;
 import fr.nakaoni.aoc.DayResponse;
 import fr.nakaoni.aoc.utils.AOCReader;
 
-public class Day01Test {
-    String EXAMPLE_FILE_PATH = "day01/example.txt";
+public class Day02Test {
+    String EXAMPLE_FILE_PATH = "day02/example.txt";
 
     @Test
     public void testPart1Response() throws Exception {
-        String expected = "11";
+        String expected = "2";
 
         Stream<String> input = AOCReader.read(Paths.get(AdventOfCodeApplicationTest.BASE_TEST_FOLDER_PATH, EXAMPLE_FILE_PATH));
 
-        DayResponse day = new Day01();
+        DayResponse day = new Day02();
 
         String result = day.getPart1Response(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    public void testPart2Response() throws Exception {
-        String expected = "31";
-
-        Stream<String> input = AOCReader.read(Paths.get(AdventOfCodeApplicationTest.BASE_TEST_FOLDER_PATH, EXAMPLE_FILE_PATH));
-
-        DayResponse day = new Day01();
-
-        String result = day.getPart2Response(input);
 
         assertEquals(expected, result);
     }
