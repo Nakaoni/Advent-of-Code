f9002ebbd5986c4df73039c147f46f0c3109d889
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-12-03 21:36:48 +0100

    2025:GO:d2:p1

---
+++ 2025/two_test.go
@@ -0,0 +1,17 @@
+package aoc
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestStarTwo(t *testing.T) {
+	input := strings.NewReader("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
+
+	want := 1227775554
+	got := GetInvalidIDsSum(input)
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
